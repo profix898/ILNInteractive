341efaffbca0171f93a8b66355678e4c00a3a924
Thilo Viereck <codedev@profix898.de>
2021-07-05 15:53:18 +0200

    - task: Prototype HTML formatter for ILNumerics arrays

--- ILNInteractive/Formatters/HtmlArrayFormatter.cs
+++ ILNInteractive/Formatters/HtmlArrayFormatter.cs
@@ -1,14 +1,28 @@
 ﻿using System;
+using System.IO;
 using ILNumerics;
 using Microsoft.DotNet.Interactive.Formatting;
 
 namespace ILNInteractive.Formatters
 {
-    public class HtmlArrayFormatter : HtmlArrayBaseFormatter, ITypeFormatter
+    public class HtmlArrayFormatter : HtmlArrayFormatterBase, ITypeFormatter
     {
         #region ITypeFormatter Members
 
-        public override Type Type => typeof(Array<>);
+        public string MimeType => "text/html";
+
+        public Type Type => typeof(Array<>);
+
+        public bool Format(FormatContext context, object instance, TextWriter writer)
+        {
+            var baseArray = instance as BaseArray;
+            if (baseArray == null)
+                return false;
+
+            FormatTable(context, baseArray, writer);
+
+            return true;
+        }
 
         #endregion
     }
