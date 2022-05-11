6d222b5cab538aa7f5e6c4d852d5eff5aacd4b58
Thilo Viereck <coding@profix898.de>
2022-05-11 14:32:42 +0200

    - feature: Export graphs as PNG, SVG or TIKZ

--- ILNInteractive/ILNInteractiveOptions.cs
+++ ILNInteractive/ILNInteractiveOptions.cs
@@ -12,7 +12,7 @@ namespace ILNInteractive
         public static int GraphSvgSizeLimit { get; set; } = 4 * 1000 * 1000; // 4M bytes
 
         /// <summary>Gets or sets the pixel size of the output graph.</summary>
-        public static Point GraphSize { get; set; } = new Point(600, 400);
+        public static Point GraphSize { get; set; } = new Point(800, 600);
 
         /// <summary>Gets or sets the default rotation of the plotCube for Surface plots (via Surf).</summary>
         public static Matrix4 GraphSurfRotation { get; set; } = Matrix4.Rotation(new Vector3(1f, 0.23f, 1), 0.7f);
